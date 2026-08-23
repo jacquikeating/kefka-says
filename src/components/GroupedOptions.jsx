9132f8cff61acf01c54e67f4c26b0c486e21eca0
jacquikeating <j.keating@live.ca>
2026-08-22 23:35:59 -1000

    feat: Adapt GroupedOptions to allow for 4 options

--- src/components/GroupedOptions.jsx
+++ src/components/GroupedOptions.jsx
@@ -1,4 +1,4 @@
-export default function GroupedOptions({ text, opt1, opt2, opt3, checkbox, setterFn }) {
+export default function GroupedOptions({ text, opt1, opt2, opt3, opt4, checkbox, setterFn }) {
 
     return (
         <div className="grouped-options">
@@ -6,14 +6,21 @@ export default function GroupedOptions({ text, opt1, opt2, opt3, checkbox, sette
             <div className="buttons">
                 <button>{opt1}</button>
                 <button>{opt2}</button>
-                {opt3 && <button>{opt3}</button>}
+                {opt3 && !opt4 && <button>{opt3}</button>}
                 {checkbox && 
                     <div className="checkbox-div">
                         <input type="checkbox" name={checkbox} />
                         <label htmlFor={checkbox}>{checkbox}</label>
                     </div>
                 }
-            </div> 
+            </div>
+            {opt4 &&
+                <div className="buttons">
+                    <span>•</span>
+                    <button>{opt3}</button>
+                    <button>{opt4}</button>
+                </div>
+            } 
         </div>
     )
 }
