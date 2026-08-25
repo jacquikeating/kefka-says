6e9ddc491fef5c32e0db99c13eb3503b18d65583
jacquikeating <j.keating@live.ca>
2026-08-24 23:35:52 -0800

    feat: Create state variables for initial debuffs

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
+import { useState } from "react"
 import SelectDebuffs from "./components/SelectDebuffs"
 
 function App() {
+  const [accel, setAccel] = useState({isReal: null, length: null})
+  const [spread, setSpread] = useState(null)
+  const [shortGaze, setShortGaze] = useState({isReal: null, isOnMe: null})
+  const [longGaze, setLongGaze] = useState({isReal: null, isOnMe: null})
+  const [water, setWater] = useState({isReal: null})
+  const [fire, setFire] = useState({isReal: null})
 
   return (
     <>
